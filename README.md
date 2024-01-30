aca2634901155b7d44cda84ac4566d46cfdb6ae6
adityamurali155 <adityamurali2@gmail.com>
2024-01-30 14:57:44 +0530

    added changes

---
+++ README.md
@@ -0,0 +1,1 @@
+Testing infra using env0.
--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
-resource "aws_instance" "demo-instance" {
-     ami = "ami-0fc5d935ebf8bc3bc"
-     instance_type = "t2.medium"
-     count = 5
+resource "aws_instance" "env0-instance" {
+     ami = "ami-0a3c3a20c09d6f377"
+     instance_type = "t2.micro"
+     tags = {
+      Name = "env0-instance"
+     }
 }
-resource "aws_s3_bucket" "demo-bucket" {
-  bucket = "demo-122312-test-bucket"
+resource "aws_s3_bucket" "env0-bucket" {
+  bucket = "env0-122312-bucket"
   tags = {
-    Name        = "Demo test bucket"
-    Environment = "test"
+    Name        = "env0-bucket"
   }
 }
