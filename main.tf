e5c7cdda9bec0f9a66ac2d957bc30c70253a3301
adityamurali155 <adityamurali2@gmail.com>
2024-01-31 18:42:47 +0530

    added changes

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-resource "aws_instance" "env0-instance" {
-     ami = "ami-0a3c3a20c09d6f377"
-     instance_type = "t2.micro"
-     tags = {
-      Name = "env0-instance"
-     }
-}
 resource "aws_s3_bucket" "env0-bucket" {
   bucket = "env0-122312-bucket"
   tags = {
